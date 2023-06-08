9d8af1020234245402ab414df2b4b5210f90de67
Andres Eduardo <andmzg97@gmail.com>
2023-06-07 23:19:39 -0400

    create template, model and url

---
+++ todo_list/base/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', TaskList.as_view(), name="tasks"),
+    path('task/<int:pk>/', TaskDetail.as_view(), name="task"),
+
+]
